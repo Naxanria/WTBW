bd48bc9aa67f3ab0c39ce1f99387645150a4a133
Naxanria <gerbrandnijk@gmail.com>
2019-11-26 18:38:25 +0100

    Iron furnace progress

---
+++ src/main/java/com/wtbw/block/BaseFurnaceBlock.java
@@ -0,0 +1,79 @@
+package com.wtbw.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
+import net.minecraft.block.RedstoneTorchBlock;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.Random;
+
+/*
+  @author: Naxanria
+*/
+public class BaseFurnaceBlock<TE extends TileEntity> extends BaseTileBlock<TE>
+{
+  public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
+  public static final BooleanProperty LIT = BlockStateProperties.LIT;
+  
+  public BaseFurnaceBlock(Properties properties, TileEntityProvider<TE> tileEntityProvider)
+  {
+    super(properties, tileEntityProvider);
+  }
+  
+  @Override
+  public BlockState getStateForPlacement(BlockItemUseContext context)
+  {
+    return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
+  }
+  
+  public BlockState rotate(BlockState state, Rotation rot)
+  {
+    return state.with(FACING, rot.rotate(state.get(FACING)));
+  }
+  
+  public BlockState mirror(BlockState state, Mirror mirrorIn)
+  {
+    return state.rotate(mirrorIn.toRotation(state.get(FACING)));
+  }
+  
+  protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder)
+  {
+    builder.add(FACING, LIT);
+  }
+  
+  @OnlyIn(Dist.CLIENT)
+  public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+    if (stateIn.get(LIT))
+    {
+      double d0 = (double)pos.getX() + 0.5D;
+      double d1 = (double)pos.getY();
+      double d2 = (double)pos.getZ() + 0.5D;
+      if (rand.nextDouble() < 0.1D)
+      {
+        worldIn.playSound(d0, d1, d2, SoundEvents.BLOCK_FURNACE_FIRE_CRACKLE, SoundCategory.BLOCKS, 1.0F, 1.0F, false);
+      }
+      
+      Direction direction = stateIn.get(FACING);
+      Direction.Axis direction$axis = direction.getAxis();
+      double d3 = 0.52D;
+      double d4 = rand.nextDouble() * 0.6D - 0.3D;
+      double d5 = direction$axis == Direction.Axis.X ? (double)direction.getXOffset() * 0.52D : d4;
+      double d6 = rand.nextDouble() * 6.0D / 16.0D;
+      double d7 = direction$axis == Direction.Axis.Z ? (double)direction.getZOffset() * 0.52D : d4;
+      worldIn.addParticle(ParticleTypes.SMOKE, d0 + d5, d1 + d6, d2 + d7, 0.0D, 0.0D, 0.0D);
+      worldIn.addParticle(ParticleTypes.FLAME, d0 + d5, d1 + d6, d2 + d7, 0.0D, 0.0D, 0.0D);
+    }
+  }
+}
---
+++ src/main/java/com/wtbw/util/NBTHelper.java
@@ -0,0 +1,54 @@
+package com.wtbw.util;
+
+import net.minecraft.nbt.CompoundNBT;
+
+/*
+  @author: Naxanria
+*/
+public class NBTHelper
+{
+  public static int getInt(CompoundNBT compoundNBT, String name)
+  {
+    return getInt(compoundNBT, name, 0);
+  }
+  
+  public static int getInt(CompoundNBT compoundNBT, String name, int defaultValue)
+  {
+    if (compoundNBT.contains(name))
+    {
+      return compoundNBT.getInt(name);
+    }
+    
+    return defaultValue;
+  }
+  
+  public static CompoundNBT getCompound(CompoundNBT compoundNBT, String name)
+  {
+    return getCompound(compoundNBT, name, new CompoundNBT());
+  }
+  
+  public static CompoundNBT getCompound(CompoundNBT compoundNBT, String name, CompoundNBT defaultValue)
+  {
+    if (compoundNBT.contains(name))
+    {
+      return compoundNBT.getCompound(name);
+    }
+    
+    return defaultValue;
+  }
+  
+  public static float getFloat(CompoundNBT compoundNBT, String name)
+  {
+    return getFloat(compoundNBT, name, 0);
+  }
+  
+  public static float getFloat(CompoundNBT compoundNBT, String name, float defaultValue)
+  {
+    if (compoundNBT.contains(name))
+    {
+      return compoundNBT.getFloat(name);
+    }
+  
+    return defaultValue;
+  }
+}
