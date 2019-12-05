c89d9ee2b7a7a5b38ec90b773f1580b2a7630363
Naxanria <gerbrandnijk@gmail.com>
2019-12-05 21:55:05 +0100

    Greenhouse glass

--- src/main/java/com/wtbw/block/GreenHouseGlass.java
+++ src/main/java/com/wtbw/block/GreenHouseGlass.java
@@ -2,10 +2,14 @@ package com.wtbw.block;
 
 import com.wtbw.util.RandomUtil;
 import net.minecraft.block.*;
+import net.minecraft.command.impl.ParticleCommand;
+import net.minecraft.item.BoneMealItem;
 import net.minecraft.item.DyeColor;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 
 import java.util.Random;
 
@@ -48,7 +52,7 @@ public class GreenHouseGlass extends AbstractGlassBlock implements IBeaconBeamCo
       {
         if (world.canBlockSeeSky(pos.up()))
         {
-          boolean grown = false;
+          // FIXME: Particles
           for (int i = 1; i < maxRange; i++)
           {
             BlockPos check = pos.down(i);
@@ -56,23 +60,18 @@ public class GreenHouseGlass extends AbstractGlassBlock implements IBeaconBeamCo
             Block block = foundState.getBlock();
             if (block instanceof CropsBlock)
             {
-              grown = true;
+              BoneMealItem.spawnBonemealParticles(world, check, rand.nextInt(7) + 5);
               ((CropsBlock) block).grow(world, check, foundState);
          
               break;
             }
             if (block instanceof SaplingBlock)
             {
-              grown = true;
+              BoneMealItem.spawnBonemealParticles(world, check, rand.nextInt(7) + 5);
               ((SaplingBlock) block).grow(world, check, foundState, rand);
 
               break;
             }
-          }
-          
-          if (grown)
-          {
-            // particles
           }
         }
       }
