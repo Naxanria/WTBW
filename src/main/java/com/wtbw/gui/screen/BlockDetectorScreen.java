14125410fbcfb5d5e1ab433ea5d1d67090f2e4fd
Naxanria <gerbrandnijk@gmail.com>
2019-12-15 11:17:19 +0100

    Finished gui for block detector

--- src/main/java/com/wtbw/gui/screen/BlockDetectorScreen.java
+++ src/main/java/com/wtbw/gui/screen/BlockDetectorScreen.java
@@ -12,7 +12,9 @@ import net.minecraft.block.BlockState;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.state.IProperty;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.translation.LanguageMap;
 import net.minecraftforge.fml.client.config.GuiButtonExt;
 
 import java.util.Collection;
@@ -23,6 +25,8 @@ import java.util.Map;
 */
 public class BlockDetectorScreen extends BaseContainerScreen<BlockDetectorContainer>
 {
+  public static final ResourceLocation GUI = new ResourceLocation("wtbw:textures/gui/block_detector.png");
+  
   private GuiButtonExt currentButton;
   private GuiButtonExt matchButton;
   
@@ -36,9 +40,9 @@ public class BlockDetectorScreen extends BaseContainerScreen<BlockDetectorContai
   {
     super.init();
   
-    currentButton = addButton(new GuiButtonExt(guiLeft + 10, guiTop + 10, 60, 22, "Set to current",
+    currentButton = addButton(new GuiButtonExt(guiLeft + 10, guiTop + 4, 60, 22, I18n.format("wtbw.gui.current"),
       button -> GuiUtil.sendButton(BlockDetectorTileEntity.BUTTON_CURRENT, container.tileEntity.getPos(), ClickType.LEFT)));
-    matchButton = addButton(new GuiButtonExt(guiLeft + 10 + 60 + 1, guiTop + 10, 60, 22, "Exact Match",
+    matchButton = addButton(new GuiButtonExt(guiLeft + 10 + 60 + 1, guiTop + 4, 60, 22, I18n.format("wtbw.gui.exact"),
       button -> GuiUtil.sendButton(BlockDetectorTileEntity.BUTTON_MATCH, container.tileEntity.getPos(), ClickType.LEFT)));
     matchButton.setFGColor(container.tileEntity.isExactMatch() ? 0xff00ff00 : 0xffff0000);
   }
@@ -57,10 +61,11 @@ public class BlockDetectorScreen extends BaseContainerScreen<BlockDetectorContai
   protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY)
   {
 //    GuiUtil.renderTexture();
-    fill(guiLeft, guiTop, guiLeft + xSize, guiTop + ySize, 0xff888888);
+//    fill(guiLeft, guiTop, guiLeft + xSize, guiTop + ySize, 0xff888888);
 //    drawString(font, "Target: " + container.tileEntity.getTarget().toString(), guiLeft + 10, guiTop + 10 + 22 + 1, 0xffffffff);
-    drawBlockState(guiLeft + 10, guiTop + 10 + 22 + 1, container.tileEntity.getTarget());
-    drawString(font, container.tileEntity.getPower() + "", guiLeft + 10, guiTop + ySize - 10, 0xff8888FF);
+    GuiUtil.renderTexture(guiLeft, guiTop, xSize, xSize, 0, 0, 256, 256, GUI);
+    drawBlockState(guiLeft + 10, guiTop + 35, container.tileEntity.getTarget());
+//    drawString(font, container.tileEntity.getPower() + "", guiLeft + 10, guiTop + ySize - 10, 0xff8888FF);
   }
   
   private void drawBlockState(int x, int y, BlockState state)
